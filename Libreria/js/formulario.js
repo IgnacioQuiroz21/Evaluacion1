e2f26e84b8df96a277a3b9c730511cd7375ba329
Ignacio Quiroz <igna.quiroz@duoc.cl>
2021-05-04 23:57:31 -0400

    ValidacionesListasParte1y2ListaFaltaApi

--- Libreria/js/formulario.js
+++ Libreria/js/formulario.js
@@ -31,72 +31,72 @@ $(function() {
    var error_fdescripcion = false;
    
 
-   $("#nFamilia").focusout(function(){
+   $("#form_familia").focusout(function(){
       check_nFamilia()
    });
-   $("#nPersonaje").focusout(function() {
+   $("#form_personaje").focusout(function() {
       check_nPersonaje();
    });
-   $("#femail").focusout(function() {
+   $("#form_email").focusout(function() {
       check_email();
    });
-   $("#descripcionTexto").focusout(function() {
+   $("#form_descripcion").focusout(function() {
       check_descripcionTexto();
    });
 
 
    function check_nFamilia() {
       var pattern = /^[a-zA-Z]*$/;
-      var Ffamilia = $("#nFamilia").val();
+      var Ffamilia = $("#form_familia").val();
       if (pattern.test(Ffamilia) && Ffamilia !== '') {
          $("#fnFamilia_error_mensaje").hide();
-         $("#nFamilia").css("border-bottom","2px solid #34F458");
+         $("#form_familia").css("border-bottom","2px solid #34F458");
       } else {
          $("#fnFamilia_error_mensaje").html("Por favor, escriba su nombre de familia.");
          $("#fnFamilia_error_mensaje").show();
-         $("#nFamilia").css("border-bottom","2px solid #F90A0A");
+         $("#form_familia").css("border-bottom","2px solid #F90A0A");
          error_fnFamilia = true;
       }
    }
 
    function check_nPersonaje() {
       var pattern = /^[a-zA-Z]*$/;
-      var FPersonaje = $("#nPersonaje").val()
+      var FPersonaje = $("#form_personaje").val()
       if (pattern.test(FPersonaje) && FPersonaje !== '') {
          $("#fnPersonaje_error_mensaje").hide();
-         $("#nPersonaje").css("border-bottom","2px solid #34F458");
+         $("#form_personaje").css("border-bottom","2px solid #34F458");
       } else {
          $("#fnPersonaje_error_mensaje").html("Por favor, escriba el nombre de su personaje.");
          $("#fnPersonaje_error_mensaje").show();
-         $("#nPersonaje").css("border-bottom","2px solid #F90A0A");
+         $("#form_personaje").css("border-bottom","2px solid #F90A0A");
          error_fnPersonaje = true;
       }
    }
 
    function check_email() {
     var pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    var Femail = $("#femail").val();
-    if (pattern.test(Femail) && Femail !== '') {
+    var email = $("#form_email").val();
+    if (pattern.test(email) && email !== '') {
        $("#femail_error_mensaje").hide();
-       $("#femail").css("border-bottom","2px solid #34F458");
+       $("#form_email").css("border-bottom","2px solid #34F458");
     } else {
        $("#femail_error_mensaje").html("Por favor, escriba un correo electrónico.");
        $("#femail_error_mensaje").show();
-       $("#femail").css("border-bottom","2px solid #F90A0A");
+       $("#form_email").css("border-bottom","2px solid #F90A0A");
        error_femail = true;
     }
    }
 
    function check_descripcionTexto() {
     
-    var FdescripcionTexto = $("#descripcionTexto").val()
+    var FdescripcionTexto = $("#form_descripcion").val()
     if ( FdescripcionTexto !== '') {
        $("#fdescripcion_error_mensaje").hide();
-       $("#descripcionTexto").css("border-bottom","2px solid #34F458");
+       $("#form_descripcion").css("border-bottom","2px solid #34F458");
     } else {
        $("#fdescripcion_error_mensaje").html("Por favor, escriba su mensaje.");
        $("#fdescripcion_error_mensaje").show();
-       $("#descripcionTexto").css("border-bottom","2px solid #F90A0A");
+       $("#form_descripcion").css("border-bottom","2px solid #F90A0A");
        error_fdescripcion = true;
     }
    }
@@ -104,10 +104,10 @@ $(function() {
 
 
    $("#formulario").submit(function() {
-    var error_fnFamilia = false;
-    var error_fnPersonaje = false;
-    var error_femail = false;
-    var error_fdescripcion = false;
+    error_fnFamilia = false;
+    error_fnPersonaje = false;
+    error_femail = false;
+    error_fdescripcion = false;
     
 
     check_nFamilia();
@@ -116,7 +116,7 @@ $(function() {
     check_descripcionTexto();
       
 
-      if (error_fnFamilia === false && error_fnPersonaje === false && error_femail === false && error_fdescripcion === false ) {
+      if (error_fnFamilia === false && error_fnPersonaje === false && error_femail === false && error_fdescripcion === false) {
          alert("Mensaje Enviado!");
          return true;
       } else {
