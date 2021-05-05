d8b57756b7ac0c3e8037f255f9432e4fa6f2372a
Ignacio Quiroz <igna.quiroz@duoc.cl>
2021-05-05 07:30:41 -0400

    EnviarInformacionGmailListoBugsListos

--- Formulario.php
+++ Formulario.php
@@ -1,17 +1,21 @@
 <?php
-if(!empty($_POST['Enviar'])){
+if(!empty($_POST)){
     $email_to = "recibir31@gmail.com";
     $email_subject= "Contacto Usuario";
 
     $email_message ="Detalles del formulario de contacto:\n\n";
-    $email_message .= "Nombre de Familia " .$_POST ['familia'] . "\n";
-    $email_message .= "Nombre de Personaje " .$_POST ['personaje'] . "\n";
-    $email_message .= "Correo de Contacto " .$_POST ['correo'] . "\n";
-    $email_message .= "Mensaje " . $_POST ['descripcion'] . "\n";
+    $email_message .= "Nombre de Familia :" .$_POST['familia'] . "\n";
+    $email_message .= "Nombre de Personaje :" .$_POST['personaje'] . "\n";
+    $email_message .= "Correo de Contacto :" .$_POST['correo'] . "\n";
+    $email_message .= "Mensaje :" . $_POST['descripcion'] . "\n";
+ 
 
 
 
- mail($email_to, $email_subject, $email_message );
+ mail($email_to, $email_subject, $email_message);
+    
+ echo "<script> location.href =history.back();</script>";
+ 
  
 }
 ?>
